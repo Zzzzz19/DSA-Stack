cb7279f2ba0c660c06c4bfa8f7424ae372a30128
janmelmel <70429539+janmelmel@users.noreply.github.com>
2022-02-23 15:57:00 +0800

    Complete

--- stackArray.h
+++ stackArray.h
@@ -12,33 +12,64 @@ void initStack(Stack *s)
     s->top = -1;
 }
 
-void push(Stack *s, char data)
-{
-    if(isFull(*s)){
-        s->data[s->top+1] = data;
-        s->top++;
-    }
-}
-
-void pop(Stack *s){
-    if (isEmpty(*s))
-    {
-    	s->data[s->top--] = NULL;
-    }
-    
-}
-
-void isFull(Stack s)
+int isFull(Stack s)
 {
 	return s.top == SIZE-1 ? 1 : 0;
 }
 
-void isEmpty(Stack s)
+int isEmpty(Stack s)
 {
 	return s.top == -1 ? 1 : 0;
 }
 
+void push(Stack *s, char data)
+{
+    if(!isFull(*s)){
+        s->data[s->top+1] = data;
+        s->top++;	
+    }
+}
+
+char pop(Stack *s)
+{
+	char ret;
+    if (!isEmpty(*s))
+    {
+    	ret = s->data[s->top];
+    	s->data[s->top--] = NULL;
+	}
+    
+    return ret;
+}
+
 void displayStack(Stack s)
 {
-	puts(s.data);
+	printf("%s\n", s.data);
 }
+
+char top(Stack s)
+{
+	return s.data[s.top];
+}
+
+void insertBottom(Stack *s, char data)
+{
+	Stack temp;
+		
+	if(!isFull(*s)){
+		initStack(&temp);
+		
+		while(!isEmpty(*s)){	            
+	 		push(&temp,pop(s));
+		}
+		
+		push(s,data);
+		
+		while(!isEmpty(temp)){
+			push(s,pop(&temp));
+		}
+	}
+}
+		
+   
+   
--- test.c
+++ test.c
@@ -6,6 +6,8 @@
 int main()
 {
     Stack stack;
+    char topCpy;
+    char popChar;
     
     initStack(&stack);
     
@@ -14,13 +16,26 @@ int main()
 	push(&stack, 'D');
 	push(&stack, 'b');
 	push(&stack, 'a');
+    push(&stack, 'E');
+    push(&stack, 'c');
+	push(&stack, 'D');
+	push(&stack, 'b');
+	push(&stack, 'a');
+	push(&stack, 'E');
 	printf("Stack before pop\n");
 	displayStack(stack);
-	pop(&stack);
-	pop(&stack);
-	pop(&stack);
-
-    printf("Stack after pop\n");
+	
+	printf("Stack after pop\n");
+	popChar = pop(&stack);
+	popChar = pop(&stack);
+	popChar = pop(&stack);
+	displayStack(stack);
+	
+//	topCpy = top(stack);
+//	printf("top %c\n", topCpy);
+//	
+	insertBottom(&stack,'K');
+	printf("Stack after insertBottom\n");
 	displayStack(stack);
 	
 	return 0;
